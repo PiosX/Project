1590646d54106a07fa08c7f999de3aabdc793a24
PiosX <piosso.pios@op.pl>
2021-07-09 18:13:51 +0200

    Front Concept

--- public/index.php
+++ public/index.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
+    <div class="top-container">
+        <nav>
+            <a href="">Home</a>
+            <a href="">Informations</a>
+            <a href="">Chat</a>
+            <a href="">Forum</a>
+            <button id="homeLog">Sign In</button>
+        </nav>      
+    </div>
+    <div class="mid-container">
     
+    </div>
+    <div class="bot-container">
+    
+    </div>
 </body>
 </html>
